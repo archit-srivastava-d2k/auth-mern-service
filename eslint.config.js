cc82758e63a81db43864e66a2cfdb7841cd8836f
Archit-D2K <i-archit.rivastava@d2kindia.com>
2025-06-13 01:59:59 +0530

    add esint

---
+++ eslint.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from "eslint/config";
+import js from "@eslint/js";
+
+export default defineConfig([
+  { ignores: ["dist", "node_modules"] },
+  {
+    files: ["**/*.js"],
+    plugins: {
+      js,
+    },
+    extends: ["js/recommended"],
+    rules: {
+      "no-unused-vars": "warn",
+      "no-undef": "warn",
+    },
+  },
+]);
