fdc8928a568781875b892e3245a9afb702f01e4e
Archit-D2K <i-archit.rivastava@d2kindia.com>
2025-06-20 00:20:09 +0530

    setup database

---
+++ src/config/data-source.ts
@@ -0,0 +1,18 @@
+import "reflect-metadata";
+import { DataSource } from "typeorm";
+import { User } from "../entity/User";
+import { serverConfig as config, serverConfig } from "./index";
+
+export const AppDataSource = new DataSource({
+  type: "postgres",
+  host: config.DB_HOST || "localhost",
+  port: Number(config.DB_PORT),
+  username: config.DB_USERNAME || "test",
+  password: config.DB_PASSWORD || "test",
+  database: config.DB_NAME || "test",
+  synchronize: serverConfig.NODE_ENV !== "prod", // Disable synchronization in production
+  logging: false,
+  entities: [User],
+  migrations: [],
+  subscribers: [],
+});
